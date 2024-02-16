998c93ad2907fea422fce3b090cc074a4a102c02
Torben Diegel <xendios@gmail.com>
2024-02-16 15:12:08 +0100

    feat: add spaceship, movement, and debug plugins
    - The spaceship plugin adds a system to spawn a spaceship entity with a spatial bundle and a velocity component.
    - The movement plugin adds a system to update the position of entities with a velocity component.
    - The debug plugin adds a system to print the position of entities with a transform component.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
 use bevy::prelude::*;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
 
-#[derive(Component, Debug)]
-pub struct Velocity {
-    pub value: Vec3,
-}
+mod debug;
+mod movement;
+mod spaceship;
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_spaceship)
-        .add_systems(Update, (update_position, print_position))
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
         .add_plugins(DefaultPlugins)
         .run();
-}
-
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((
-        SpatialBundle::default(),
-        Velocity {
-            value: Vec3::new(0., 0., 0.),
-        },
-    ));
-}
-
-fn update_position(mut query: Query<(&Velocity, &mut Transform)>) {
-    for (velocity, mut position) in query.iter_mut() {
-        position.translation.x += velocity.value.x;
-        position.translation.y += velocity.value.y;
-        position.translation.z += velocity.value.z;
-    }
-}
-
-fn print_position(query: Query<(Entity, &Transform)>) {
-    for (entity, transform) in query.iter() {
-        info!("Entity: {:?} is at position: {:?}", entity, transform);
-    }
 }
